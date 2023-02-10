ed558b66975adc79d3d433d3a0b2888a4c4e8451
Lê Văn Tuân <tuanlv202@gmail.com>
2023-02-10 13:01:43 +0700

    horse racing logic

--- src/race.js
+++ src/race.js
@@ -1,5 +1,19 @@
+import httpRequest from "./utils/httpRequest";
+
 const race = async (horses) => {
-  // TODO your code here.
+  // making ajax request to json files
+  const res = await Promise.all(horses.map((horse) => httpRequest.get(horse.url)));
+
+  // sort response => get ajax request finished first
+  res.sort((prev, next) => prev.duration - next.duration);
+  const firstRes = res.shift();
+  const reqUrl = firstRes.request.responseURL.slice(firstRes.request.responseURL.indexOf("horses"));
+  const winnerHorse = horses.find((horse) => horse.url === reqUrl);
+
+  return {
+    name: winnerHorse.name,
+    image: firstRes.data.image,
+  };
 };
 
 export default race;
